91418711f0ed969b1679a5a8d1ff3b22c909bace
sujeongHeo401 <gotnwjd40@gmail.com>
2022-04-03 22:45:02 +0900

    commit

---
+++ go-bookstore/cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/akhil/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/sujeongHeo401/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+}
